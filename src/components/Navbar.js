2cc24aca98902e7018ef0e2f492280788d01f1ab
Ricky Kuo <rickykuo925@gmail.com>
2022-12-09 17:47:46 +0000

    navbar done

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,15 +5,16 @@ import { FaBars, FaTimes } from "react-icons/fa";
 import { IconContext } from "react-icons/lib";
 
 const Navbar = () => {
-  const [click, setclick] = useState(false);
+  const [click, setClick] = useState(false);
 
-  const handleClick = () => setclick(!click);
+  const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
 
   return (
     <IconContext.Provider value={{ color: "#fff" }}>
       <div className="navbar">
         <div className="navbar-container container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Wonderly Time
           </Link>
           <div className="menu-icon" onClick={handleClick}>
@@ -21,17 +22,17 @@ const Navbar = () => {
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/london" className="nav-link">
+              <Link to="/london" className="nav-link" onClick={closeMobileMenu}>
                 London
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/paris" className="nav-link">
+              <Link to="/paris" className="nav-link" onClick={closeMobileMenu}>
                 Paris
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="rome" className="nav-link">
+              <Link to="rome" className="nav-link" onClick={closeMobileMenu}>
                 Rome
               </Link>
             </li>
