08cd5be82e68cc14f35b9a673594d02d4ba906f9
Ricky Kuo <rickykuo925@gmail.com>
2022-12-09 17:41:08 +0000

    nav-link styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 const Navbar = () => {
   const [click, setclick] = useState(false);
@@ -9,33 +10,35 @@ const Navbar = () => {
   const handleClick = () => setclick(!click);
 
   return (
-    <div className="navbar">
-      <div className="navbar-container container">
-        <Link to="/" className="navbar-logo">
-          Wonderly Time
-        </Link>
-        <div className="menu-icon" onClick={handleClick}>
-          {click ? <FaTimes /> : <FaBars />}
+    <IconContext.Provider value={{ color: "#fff" }}>
+      <div className="navbar">
+        <div className="navbar-container container">
+          <Link to="/" className="navbar-logo">
+            Wonderly Time
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            {click ? <FaTimes /> : <FaBars />}
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/london" className="nav-link">
+                London
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/paris" className="nav-link">
+                Paris
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="rome" className="nav-link">
+                Rome
+              </Link>
+            </li>
+          </ul>
         </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="/london" className="nav-link">
-              London
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/paris" className="nav-link">
-              Paris
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="rome" className="nav-link">
-              Rome
-            </Link>
-          </li>
-        </ul>
       </div>
-    </div>
+    </IconContext.Provider>
   );
 };
 
