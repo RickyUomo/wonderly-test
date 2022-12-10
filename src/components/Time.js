913b3e662742ad677a61a2d655ecf6b506aefe5a
Ricky Kuo <rickykuo925@gmail.com>
2022-12-10 15:32:30 +0000

    Time css

--- src/components/Time.js
+++ src/components/Time.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Button from "./Button";
+import "./time.css";
 
 const Time = ({ city, time }) => {
   return (
-    <div>
-      <div className="city">{city}</div>
+    <div className="city-container">
+      <div className="city-name">{city}</div>
       <div className="city-time">{time}</div>
-      <Button>Refresh Time</Button>
+      <div className="city-btn">
+        <Button>Refresh Time</Button>
+      </div>
     </div>
   );
 };
